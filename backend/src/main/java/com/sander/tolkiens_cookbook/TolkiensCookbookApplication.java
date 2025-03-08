f185781a33fe67ecc941fc46babe4b3e20935b27
Sander Arjan Benoist <sabenoist5@gmail.com>
2025-03-08 20:24:13 +0100

    prepared initial Java Spring Boot back-end

---
+++ backend/src/main/java/com/sander/tolkiens_cookbook/TolkiensCookbookApplication.java
@@ -0,0 +1,13 @@
+package com.sander.tolkiens_cookbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TolkiensCookbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TolkiensCookbookApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/sander/tolkiens_cookbook/TolkiensCookbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sander.tolkiens_cookbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TolkiensCookbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
