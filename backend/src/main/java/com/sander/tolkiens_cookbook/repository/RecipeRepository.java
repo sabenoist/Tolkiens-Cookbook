033574fca8ae96a384ade29f4ba6f2d1d35453f1
Sander Arjan Benoist <sabenoist5@gmail.com>
2025-03-12 22:26:56 +0100

    cleaned up and optimized filter search query

--- backend/src/main/java/com/sander/tolkiens_cookbook/repository/RecipeRepository.java
+++ backend/src/main/java/com/sander/tolkiens_cookbook/repository/RecipeRepository.java
@@ -25,21 +25,24 @@ public interface RecipeRepository extends JpaRepository<Recipe, Integer> {
      */
     Recipe findByName(String name);
 
-    @Query(value = "SELECT DISTINCT r.* FROM recipes r " +
-            "JOIN recipe_ingredients ri ON r.recipe_id = ri.recipe_id " +
-            "JOIN ingredients i ON i.ingredient_id = ri.ingredient_id " +
-            "WHERE " +
-            "(:includeEmpty OR i.ingredient_name IN (:includeIngredients)) " +
-            "AND (:excludeEmpty OR r.recipe_id NOT IN ( " +
-            "   SELECT r2.recipe_id FROM recipes r2 " +
-            "   JOIN recipe_ingredients ri2 ON r2.recipe_id = ri2.recipe_id " +
-            "   JOIN ingredients i2 ON i2.ingredient_id = ri2.ingredient_id " +
-            "   WHERE i2.ingredient_name IN (:excludeIngredients) " +
-            ")) " +
-            "AND (:keywordPattern IS NULL OR LOWER(CAST(r.instructions AS TEXT)) LIKE LOWER(:keywordPattern)) " +
-            "AND (:servings IS NULL OR r.servings = :servings) " +  // Added filter for servings
-            "GROUP BY r.recipe_id " +
-            "HAVING COUNT(DISTINCT CASE WHEN i.ingredient_name IN (:includeIngredients) THEN i.ingredient_name END) = :includeCount",
+    @Query(value = "SELECT DISTINCT r.* \n" +
+            "FROM recipes r\n" +
+            "JOIN recipe_ingredients ri ON r.recipe_id = ri.recipe_id\n" +
+            "JOIN ingredients i ON i.ingredient_id = ri.ingredient_id\n" +
+            "LEFT JOIN (\n" +
+            "    SELECT r2.recipe_id\n" +
+            "    FROM recipes r2\n" +
+            "    JOIN recipe_ingredients ri2 ON r2.recipe_id = ri2.recipe_id\n" +
+            "    JOIN ingredients i2 ON i2.ingredient_id = ri2.ingredient_id\n" +
+            "    WHERE i2.ingredient_name IN (:excludeIngredients)\n" +
+            ") excluded ON excluded.recipe_id = r.recipe_id\n" +
+            "WHERE\n" +
+            "(:includeEmpty OR i.ingredient_name IN (:includeIngredients))\n" +
+            "AND (excluded.recipe_id IS NULL OR :excludeEmpty)\n" +
+            "AND (:keywordPattern IS NULL OR LOWER(r.instructions) LIKE LOWER(:keywordPattern))\n" +
+            "AND (:servings IS NULL OR r.servings = :servings)\n" +
+            "GROUP BY r.recipe_id\n" +
+            "HAVING COUNT(DISTINCT CASE WHEN i.ingredient_name IN (:includeIngredients) THEN i.ingredient_name END) = :includeCount\n",
             nativeQuery = true)
     List<Recipe> searchRecipes(
             @Param("includeIngredients") List<String> includeIngredients,
